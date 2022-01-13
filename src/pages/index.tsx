029d88e3be6f6eb70cab5105d83e833b7faba7db
Marciosergios <marciosergios2020@gmail.com>
2022-01-13 07:56:18 -0300

    Adding styles into scss files and customizing the home page.

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+import styles from '../styles/global.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | iz.news</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
